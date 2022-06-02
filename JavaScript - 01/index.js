475246005dd3289bbafa39a655a1235a5a460e55
yanvictorsn <yanvictosn@gmail.com>
2022-06-02 11:23:15 -0300

    chore:add javascript file

---
+++ JavaScript - 01/index.js
@@ -0,0 +1,8 @@
+let mainTitle = document.querySelector("h1")
+mainTitle.textContent ="Olá, Mundo!"
+
+let mainButton = document.querySelector("button")
+
+mainButton.onclick = function () {
+    console.log ("Olá, Console!");
+}
