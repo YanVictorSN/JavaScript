09101cb1b82645d69e0b04d17316ef2221d2c28c
yanvictorsn <yanvictosn@gmail.com>
2022-06-02 13:29:15 -0300

    fix:add src img

--- JavaScript - 01.a/index.js
+++ JavaScript - 01.a/index.js
@@ -2,9 +2,9 @@ let mainImage = document.querySelector("img");
 
 mainImage.onclick = function() {
     let mainSrc = mainImage.getAttribute("src")
-    if (mainSrc === "/assets/imgs/lobo.png") {
-        mainImage.setAttribute("src","/assets/imgs/lua.png")
+    if (mainSrc === "../assets/imgs/lobo.png") {
+        mainImage.setAttribute("src","../assets/imgs/lua.png")
     } else {
-        mainImage.setAttribute ("src","/assets/imgs/lobo.png" )
+        mainImage.setAttribute ("src","../assets/imgs/lobo.png" )
     }
 }
