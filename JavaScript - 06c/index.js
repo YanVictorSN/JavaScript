575c1b561f0af945ffdb9d6ae9006ef0e8cffbd8
yanvictorsn <yanvictosn@gmail.com>
2022-06-10 21:43:58 -0300

    chore: second commit javascript 6

--- JavaScript - 06c/index.js
+++ JavaScript - 06c/index.js
@@ -1,28 +1,46 @@
-let minutes = document.getElementById("minutes").selectedIndex;
-
-let seconds = document.getElementById("seconds").selectedIndex;
 
 let button = document.getElementById("alarm");
 
 let mensage = document.getElementById("time-left");
 
-let minutesTime = 59;
-
-let secondsTime = 59;
+let alarmSound = new Audio ("assets/198841__bone666138__analog-alarm-clock.wav")
 
 
 button.addEventListener ("click", count);
 
 function count() {
-    minutesTime = setInterval(countMinutes,1000);
+    minutesTime = setInterval(countMinutes, 1000);
+    secondsTime = setInterval(countSeconds, 1000);
+    minutes = document.getElementById("minutes").selectedIndex;
+    seconds = document.getElementById("seconds").selectedIndex;
+    button.addEventListener ("click", desarmAlarm);
+    button.textContent = "Desativar Alarme";
+    button.removeEventListener ("click", count);
+    console.log(seconds)
 }
 
-console.log(minutesTime)
+function desarmAlarm() {
+    clearInterval(minutes);
+    clearInterval(seconds);
+    mensage.textContent = `Tempo restante: ${minutes}:${seconds} `;
+}
+
 function countMinutes() {
-    console.log(minutesTime)
-    if (--minutesTime > 0) {
-        mensage.textContent = `${minutesTime}`;
-    } else if (minutesTime == 0) {
-        console.log("bip,bip")
+    mensage.textContent = `Tempo restante: ${minutes}:${seconds} `
+    if (minutes > 0) {
+        --minutes;
+    } else if (minutes == 0 && seconds== 0) {
+        alarmSound.play();
+    }
+}
+function countSeconds() {
+    mensage.textContent = `Tempo restante: ${minutes}:${seconds} `
+    if (seconds > 0) {
+        --seconds;
+    } else if (minutes > 0 && seconds == 0) {
+        seconds= 59;
+    } else if (minutes == 0 && seconds == 0) {
+       clearInterval(minutes);
+       clearInterval(seconds);
     }
 }
